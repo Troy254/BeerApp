7fdf08478cd7e66c574502db83e8e60a1160970a
Troy254 <njerubrianmunene@gmail.com>
2023-08-09 09:36:37 +0300

    customer controller

---
+++ src/main/java/com/springframework/spring6restmvc/controllers/CustomerController.java
@@ -0,0 +1,52 @@
+package com.springframework.spring6restmvc.controllers;
+
+import com.springframework.spring6restmvc.model.Customer;
+import com.springframework.spring6restmvc.services.CustomerService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    CustomerService customerService;
+    @DeleteMapping("{customerId}")
+    public ResponseEntity deleteById(@PathVariable("customerId") UUID customerId){
+        customerService.deleteCustomerById(customerId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+    @PutMapping("{customerId}")
+    public ResponseEntity updateById(@PathVariable("customerId") UUID customerId, @RequestBody Customer customer){
+        customerService.updateCustomerById(customerId,customer);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping
+    public ResponseEntity postHandle(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.saveNewCustomer(customer);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Customer> listCustomers(){
+        return customerService.listCustomers();
+    }
+    @RequestMapping("{customerId}")
+    public Customer getCustomerById(@PathVariable("customerId") UUID customerId){
+        log.debug("Get Customer By Id - in Controller");
+        return customerService.getCustomerById(customerId);
+
+    }
+}

27623e66894bd782a75a1dcf19f3ef9e6274a339
Troy254 <andatisiro@gmail.com>
2023-08-09 16:49:24 +0300

    Controller fix void getBeerById()

--- src/main/java/com/springframework/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/springframework/spring6restmvc/controllers/BeerController.java
@@ -14,11 +14,11 @@ import java.util.UUID;
 
 @Slf4j
 @RestController
-@AllArgsConstructor
-@RequestMapping("/api/v1/beer")
+ @AllArgsConstructor
+ @RequestMapping("/api/v1/beer")
 public class BeerController {
 
-    public static final String BEER_PATH = " ";
+     public static final String BEER_PATH = " ";
     private BeerService beerService;
 
     @DeleteMapping("{beerId}")
@@ -28,16 +28,16 @@ public class BeerController {
     }
 
     @PutMapping("{beerId}")
-    public ResponseEntity updateById(@PathVariable("beerId") UUID beerId,@RequestBody Beer beer){
-        beerService.updateBeerById(beerId,beer);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+     public ResponseEntity updateById(@PathVariable("beerId") UUID beerId,@RequestBody Beer beer){
+         beerService.updateBeerById(beerId,beer);
+         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping
     public ResponseEntity postHandle(@RequestBody Beer beer){
         Beer savedBeer = beerService.saveNewBeer(beer);
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/api/v1/beer" + savedBeer.getId().toString());
+         headers.add("Location", "/api/v1/beer" + savedBeer.getId().toString());
         return new ResponseEntity(headers,HttpStatus.CREATED);
     }
 
@@ -49,7 +49,7 @@ public class BeerController {
 
     @RequestMapping("{beerId}")
      public Beer getBeerById(@PathVariable("beerId") UUID beerId){
-        log.debug("Get Beer By Id - in Controller");
+         log.debug("Get Beer By Id - in Controller");
         return beerService.getBeerById(beerId);
     }
 }

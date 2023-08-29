51de1519fb93e3904cc0fbb44b8a7d8ee4d6295b
Troy254 <andatisiro@gmail.com>
2023-08-29 12:54:21 +0300

    beerCsv record

--- src/main/java/com/springframework/spring6restmvc/model/BeerCSVRecord.java
+++ src/main/java/com/springframework/spring6restmvc/model/BeerCSVRecord.java
@@ -1,5 +1,24 @@
 package com.springframework.spring6restmvc.model;
 
-public class BeerCSVRecord {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BeerCSVRecord {
+  private Integer row;
+  private Integer count;
+  private String abv;
+  private String ibu;
+  private Integer id;
+  private String beer;
+  private String style;
+  private Integer breweryId;
+  private float ounces;
+  private String style2;
+  private String city;
+  private String state;
+  private String label;
 }

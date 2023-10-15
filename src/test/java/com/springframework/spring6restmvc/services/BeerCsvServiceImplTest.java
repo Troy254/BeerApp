a82db1a985e6de1366c6df1f07fc719823e57ed5
Troy254 <andatisiro@gmail.com>
2023-10-15 13:12:21 +0300

    bootstrap and BeerCsvService

---
+++ src/test/java/com/springframework/spring6restmvc/services/BeerCsvServiceImplTest.java
@@ -0,0 +1,28 @@
+package com.springframework.spring6restmvc.services;
+
+import com.springframework.spring6restmvc.model.beerCSVRecord;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BeerCsvServiceImplTest {
+
+    BeerCsvService beerCsvService = new BeerCsvServiceImpl();
+
+    @Test
+    void convertCSV() throws FileNotFoundException {
+
+        File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
+
+        List<beerCSVRecord> recs = beerCsvService.convertCSV(file);
+
+        System.out.println(recs.size());
+
+        assertThat(recs.size()).isGreaterThan(0);
+    }
+}
